930d3afd3dc765085adbe7e45001c72a4d047591
aurel fontaine <ajfame11@gmail.com>
2020-04-01 05:44:16 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -3,9 +3,9 @@ require 'pry'
 def my_find(collection)
   i = 0
   while i < collection.length
-     i = i + 1
+    
   if  yield(collection[i])
-   
+    i = i + 1
   else
     return collection[i]
 end
